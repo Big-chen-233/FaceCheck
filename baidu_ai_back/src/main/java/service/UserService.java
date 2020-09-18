13fa4e8fc7f5f36458ad1db76aea7a57393c3f33
Big-Chen-233 <1609926534@qq.com>
2020-09-18 18:56:07 +0800

    9/18

---
+++ baidu_ai_back/src/main/java/service/UserService.java
@@ -0,0 +1,22 @@
+package service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import xyz.chen.baidu_ai_back.dao.UserDao;
+import xyz.chen.baidu_ai_back.pojo.User;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    UserDao userDao;
+
+    public void add(User user){
+        userDao.save(user);
+    }
+
+    public List<User> getList(Double beauty){
+       return userDao.findByBeautyBetween(beauty-5,beauty+5);
+    }
+}
---
+++ baidu_ai_back/src/main/java/xyz/chen/baidu_ai_back/dao/UserDao.java
@@ -0,0 +1,10 @@
+package xyz.chen.baidu_ai_back.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import xyz.chen.baidu_ai_back.pojo.User;
+
+import java.util.List;
+
+public interface UserDao extends JpaRepository<User,Integer> {
+    List<User> findByBeautyBetween(double start,double end);
+}

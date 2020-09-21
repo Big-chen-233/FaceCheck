fe5489eb19261e6030014b9c8b9ce57a265e9b9f
unknown <754658150@qq.com>
2020-09-21 23:21:07 +0800

    update

--- frontend/pages/rankpage.vue
+++ frontend/pages/rankpage.vue
@@ -1,15 +1,43 @@
 <template>
+  <div >
 
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data(){
-
+    return{
+      name:'',
+      score:'',
+    }
   },
+  mounted(){
+    this.init();
+  },
+  methods:{
+    init(){
+      let data = {
+        'nickname':this.name,
+        'beauty':this.score
+      }
+      axios({
+        method:'get',
+        url:'http://localhost:8080/api/getlist',
+        data:data,
+      }).then(res=>{
+
+      }).catch((err)=>{
+
+      })
+    }
+  }
 }
 </script>
 
 <style>
+.container{
 
+}
 </style>

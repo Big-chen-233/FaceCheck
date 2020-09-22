c8e5c2447913ec835b4bcb6047041d3fb0f681f5
unknown <754658150@qq.com>
2020-09-22 10:02:12 +0800

    update

--- frontend/pages/rankpage.vue
+++ frontend/pages/rankpage.vue
@@ -3,9 +3,9 @@
     <div class="rankhead"> <div class="ranktitle"> 排行榜</div> <img  class="img"  src="@/assets/medal.png" alt="图片加载失败"></div>
     <div class="ranklist" v-for="(item,index) in users" :key="index" :v-if="index < 10">
       <div class="everylist">
-        <div class="medal1" v-if="index == 0"><img src="@/assets/fitst4.png" alt=""></div>
-        <div class="medal2" v-else-if="index == 1"><img src="@/assets/second2.png" alt=""></div>
-        <div class="medal3" v-else-if="index == 2"><img src="@/assets/third3.png" alt=""></div>
+        <div class="medal1" v-if="index == 0"></div>
+        <div class="medal2" v-else-if="index == 1"></div>
+        <div class="medal3" v-else-if="index == 2"></div>
         <div class="medal4" v-else>{{item.index}}</div>
         <div class="nickname">{{item.nickname}}</div>
         <div class="rankscore">{{item.beauty}}</div>
@@ -115,16 +115,19 @@ export default {
 .medal1{
   width: 4rem;
   height: 4rem;
+  background-image: url('@/assets/fitst4.png');
 }
 
 .medal2{
   width: 4rem;
   height: 4rem;
+  background-image: url('@/assets/second2.png');
 }
 
 .medal3{
   width: 4rem;
   height: 4rem;
+  background-image: url('@/assets/third3.png');
 }
 
 .medal4{
